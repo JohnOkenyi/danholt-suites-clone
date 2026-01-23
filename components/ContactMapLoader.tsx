0075046951fb0e228a6454fa6cc01ff8a49dbca4
John Okenyi <168378093+JohnOkenyi@users.noreply.github.com>
2026-01-23 13:14:35 -0500

    Fix: Simplify ContactMapLoader to remove ErrorBoundary wrapper

--- components/ContactMapLoader.tsx
+++ components/ContactMapLoader.tsx
@@ -1,18 +1,6 @@
 'use client'
 
-import React from 'react'
-import dynamic from 'next/dynamic'
-import ErrorBoundary from '@/components/ErrorBoundary'
-
-const ContactMap = dynamic(() => import('./ContactMap'), {
-    ssr: false,
-    loading: () => <div className="h-[400px] w-full bg-gray-100 rounded-2xl animate-pulse flex items-center justify-center text-gray-400">Loading Map...</div>
-})
+import ContactMap from './ContactMap'
 
 export default function SafeContactMap() {
-    return (
-        <ErrorBoundary>
-            <ContactMap />
-        </ErrorBoundary>
-    )
-}
+  return <ContactMap />
