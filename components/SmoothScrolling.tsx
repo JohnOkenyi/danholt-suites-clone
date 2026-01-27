baa64823bd9d74c401616f18f649a4722befd15d
John <okenyijohn@gmail.com>
2026-01-27 18:40:54 -0500

    Optimize Perf: Disable noise overlay and tune scroll physics

--- components/SmoothScrolling.tsx
+++ components/SmoothScrolling.tsx
@@ -5,13 +5,13 @@ import { ReactLenis } from '@studio-freight/react-lenis'
 export default function SmoothScrolling({ children }: { children: React.ReactNode }) {
     return (
         <ReactLenis root options={{
-            lerp: 0.1,
-            // duration: 1.5, // Removing fixed duration to rely on lerp for responsiveness
+            lerp: 0.12, // Faster response (less "laggy" feel)
+            // duration: 1.5,
             smoothWheel: true,
             orientation: 'vertical',
             gestureOrientation: 'vertical',
             touchMultiplier: 2,
-            wheelMultiplier: 1.2, // Slightly increase wheel speed for responsiveness
+            wheelMultiplier: 1.0, // More natural scroll speed
         }}>
             {children}
         </ReactLenis>
