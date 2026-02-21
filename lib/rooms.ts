a32cdab9809b8f5fcf8d31b17af3671d47fe3480
John <okenyijohn@gmail.com>
2026-02-21 11:07:53 -0500

    Update Executive Deluxe room images with new provided pictures

--- lib/rooms.ts
+++ lib/rooms.ts
@@ -47,8 +47,8 @@ export const ROOMS: Room[] = [
         guests: 3,
         size: 45,
         bedType: "Super King Bed",
-        image: "/images/room-executive.jpg",
-        images: ["/images/room-executive.jpg"],
+        image: "/images/room-executive-deluxe-new-1.jpg",
+        images: ["/images/room-executive-deluxe-new-1.jpg", "/images/room-executive-deluxe-new-2.jpg"],
         description: "The pinnacle of luxury. Expansive living space, exclusive access, and unparalleled attention to detail.",
         amenities: [
             "Free WiFi",
